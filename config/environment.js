ae67c256054f27e407704a7a98170355f513a856
Stephen Holsapple <sholsapp@gmail.com>
2015-12-26 20:42:32 -0800

    Added bootstrap theme to the ember-cli build process.

--- config/environment.js
+++ config/environment.js
@@ -16,10 +16,10 @@ module.exports = function(environment) {
     contentSecurityPolicy: {
       'default-src': "'none'",
       'script-src': "'self'",
-      'font-src': "'self'",
+      'font-src': "'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com ",
       'connect-src': "'self'",
       'img-src': "'self'",
-      'style-src': "'self' 'unsafe-inline' ",
+      'style-src': "'self' 'unsafe-inline' fonts.googleapis.com ",
       'media-src': "'self'"
     },
 
