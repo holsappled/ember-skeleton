0356ccfbdfa670d54e43e58ef448ee5f6f94e62e
Stephen Holsapple <sholsapp@gmail.com>
2016-07-08 14:50:17 -0700

    Turn on the auth/z classes for all routes.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,16 +1,18 @@
 import Ember from 'ember';
 import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 
-var RestlessAdapter = DS.RESTAdapter.extend({
+var RestlessAdapter = DS.RESTAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:custom',
   shouldReloadAll: function() {
     return true;
   },
-  host: 'http://localhost:5000',
+  host: 'http://127.0.0.1:5000',
   namespace: 'api',
   pathForType: function(type) {
     return Ember.String.underscore(type);
-  }
+  },
 });
 
 export default RestlessAdapter;
